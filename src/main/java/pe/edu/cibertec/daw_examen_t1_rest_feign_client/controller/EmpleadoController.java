0181faaf0811142f11c7ee6b4435f7b1645d4dfc
Natha23XO <hugocastroleon2301@gmail.com>
2024-09-27 20:19:16 -0500

    terminando rest

--- src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/controller/EmpleadoController.java
+++ src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/controller/EmpleadoController.java
@@ -1,8 +1,9 @@
 package pe.edu.cibertec.daw_examen_t1_rest_feign_client.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.daw_examen_t1_rest_feign_client.model.Empleado;
 import pe.edu.cibertec.daw_examen_t1_rest_feign_client.service.EmpleadoService;
 
@@ -10,11 +11,33 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/empleado")
 public class EmpleadoController {
     private final EmpleadoService empleadoService;
 
-    @GetMapping("/empleados")
-    public List<Empleado> obtenerEmpleados() {
-        return empleadoService.obtenerEmpleados();
+    @GetMapping("/findAll")
+    private ResponseEntity<List<Empleado>> findAll() {
+        return new ResponseEntity<>(this.empleadoService.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/find/{id}")
+    private ResponseEntity<Empleado> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(this.empleadoService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    private ResponseEntity<Empleado> create(@RequestBody Empleado empleado) {
+        return new ResponseEntity<>(this.empleadoService.create(empleado), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    private ResponseEntity<Empleado> update(@RequestBody Empleado empleado, @PathVariable Long id) {
+        return new ResponseEntity<>(this.empleadoService.update(empleado, id), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    private ResponseEntity<String> delete(@PathVariable Long id) {
+        return new ResponseEntity<>(this.empleadoService.delete(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/model/Empleado.java
+++ src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/model/Empleado.java
@@ -1,9 +1,16 @@
 package pe.edu.cibertec.daw_examen_t1_rest_feign_client.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
     public String nombre;
     public String apellido;
