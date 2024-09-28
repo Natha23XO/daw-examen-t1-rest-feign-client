86f905fc36b4b483567e1f09d6adb3f4e1197b19
Natha23XO <hugocastroleon2301@gmail.com>
2024-09-27 19:49:57 -0500

    empleado

--- src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/controller/EmpleadoController.java
+++ src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/controller/EmpleadoController.java
@@ -1,4 +1,20 @@
 package pe.edu.cibertec.daw_examen_t1_rest_feign_client.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.daw_examen_t1_rest_feign_client.model.Empleado;
+import pe.edu.cibertec.daw_examen_t1_rest_feign_client.service.EmpleadoService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class EmpleadoController {
+    private final EmpleadoService empleadoService;
+
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados() {
+        return empleadoService.obtenerEmpleados();
+    }
 }
--- src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/service/EmpleadoService.java
+++ src/main/java/pe/edu/cibertec/daw_examen_t1_rest_feign_client/service/EmpleadoService.java
@@ -1,4 +1,41 @@
 package pe.edu.cibertec.daw_examen_t1_rest_feign_client.service;
 
-public class EmpleadoService {
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.daw_examen_t1_rest_feign_client.model.Empleado;
+import pe.edu.cibertec.daw_examen_t1_rest_feign_client.repository.EmpleadoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmpleadoService implements EmpleadoRepository {
+
+    @Override
+    public List<Empleado> obtenerEmpleados() {
+        List<Empleado> empleados = new ArrayList<>();
+
+        Empleado empleado1 = new Empleado();
+        empleado1.setId(1);
+        empleado1.setNombre("Hugo Nathanael");
+        empleado1.setApellido("Castro Leon");
+        empleado1.setPuesto("Desarrollador Web");
+
+        Empleado empleado2 = new Empleado();
+        empleado2.setId(2);
+        empleado2.setNombre("Genesis Giovanna");
+        empleado2.setApellido("Despoux Arica");
+        empleado2.setPuesto("Analista de Datos");
+
+        Empleado empleado3 = new Empleado();
+        empleado3.setId(3);
+        empleado3.setNombre("Patrick Alexander");
+        empleado3.setApellido("Montes de Oca");
+        empleado3.setPuesto("Ingeniero de Datos");
+
+        empleados.add(empleado1);
+        empleados.add(empleado2);
+        empleados.add(empleado3);
+
+        return empleados;
+    }
 }
